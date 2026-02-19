d8a8c4775655f0f7c47fa59413d9c46e78f0c616
Apha-Base <Apha-Base>
2026-02-20 00:12:39 +0100

    Initial Commit

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public Guid RoleId { get; set; }
+        public int PhoneNumber { get; set; }
+        public bool isActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime LastLoginDate { get; set; }
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime RefreshTokenExpiryTime { get; set; }
+    }
+}
